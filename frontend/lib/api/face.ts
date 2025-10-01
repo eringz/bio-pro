dbacd4a7de60bc41fdec205228d4281270a637d7
unknown <ron.garcia.santos@gmail.com>
2025-10-02 04:46:33 +0800

    For initial deployment

--- frontend/lib/api/face.ts
+++ frontend/lib/api/face.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://bio-pro-9pht.onrender.com";
+// const BASE_URL = "http://localhost:5000";
 
 // Registration API
 export async function uploadFace(image: string) {
