ae6a2a2db9ddf5b8f1558e4a590ccc54ef98df4f
unknown <ron.garcia.santos@gmail.com>
2025-10-03 01:17:43 +0800

    Adding MobileNav and FE RWD

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
                 </div>   
 
                 <div className="xl:hidden">
-                
+                    <MobileNav />
                 </div>             
             </div>
         </header>
