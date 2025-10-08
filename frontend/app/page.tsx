9c980e64bc562f08eae76793dc1d5a8744815e2c
unknown <ron.garcia.santos@gmail.com>
2025-10-08 23:41:07 +0800

    update Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home () {
 
   return (
     <main className="gtid grid-cols-2 gap-12 justify-between md:grid-cols-2 sm:grid-cols-1 ">
-      
-
+        Welcome to Bio Pro
     </main>
   );
 }
